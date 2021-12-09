baf613442ca9983aeb68fa72b734498ab0004dcc
Justas Cepaitis <justascepaitis@gmail.com>
2021-12-09 13:53:57 +0000

    updated level, added kelp (lower res :/), updated particles a bit

---
+++ game/kelp.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/game.tilesource\"\n"
+  "default_animation: \"kelp\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: -10.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
